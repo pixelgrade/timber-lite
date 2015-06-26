c5844b2985df4eeb19220aeb05525c9875a7db8c
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-06-26 11:36:17 +0300

    changed categories/types on featured porjects

--- template-parts/content-portfolio-featured.php
+++ template-parts/content-portfolio-featured.php
@@ -4,14 +4,14 @@
  *
  * @package Timber
  */
-//project category list
-
-$separate_meta = _x( ', ', 'Used between list items, there is a space after the comma.', 'timber' );
 ?>
+
 <div class="project-slide  rsContent">
 	<span class="vertical-title left-align"><?php the_title(); ?></span>
 	<div class="project-slide__content">
-		<?php echo get_the_term_list( $post->ID, 'jetpack-portfolio-type', '<a href="#" class="project-slide__text">', $separate_meta, '</a>' ); ?>
+
+		<?php timber_the_project_types( get_the_ID(), '<div class="portfolio_types">', '</div>' ); ?>
+
 		<a href="<?php the_permalink(); ?>" class="project-slide__link" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'timber' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
 			<h1 class="project-slide__title"><?php the_title(); ?></h1>
 			<span class="project-slide__text"><?php _e( '&#8594; View Project', 'timber' ); ?></span>
