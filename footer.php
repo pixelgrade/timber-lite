7f60402d52fa490bcffb3c2237897ab5a048f405
cristianfrumusanu <cristian.frumusanu@pixelgrade.com>
2015-06-30 08:52:54 +0300

    added overlay wrapper | #11

--- footer.php
+++ footer.php
@@ -13,6 +13,28 @@
 <?php //if ( is_page() || is_single() && ! get_post_type() == 'jetpack-portfolio' ) get_template_part( 'template-parts/content', 'single-footer' ); ?>
 
 </div><!-- #page -->
+<div class="overlay">
+	<div class="overlay__wrapper">
+		<div class="overlay__flex">
+			<?php if ( is_active_sidebar( 'overlay-widget-area-1' ) ) : ?>
+			<div class="overlay__col1">
+				<?php dynamic_sidebar( 'overlay-widget-area-1' ); ?>
+			</div>
+			<?php endif; ?>
+			<?php if ( is_active_sidebar( 'overlay-widget-area-2' ) ) : ?>
+			<div class="overlay__col2">
+				<?php dynamic_sidebar( 'overlay-widget-area-2' ); ?>
+			</div>
+			<?php endif; ?>
+			<?php if ( is_active_sidebar( 'overlay-widget-area-3' ) ) : ?>
+			<div class="overlay__col3">
+				<?php dynamic_sidebar( 'overlay-widget-area-3' ); ?>
+			</div>
+			<?php endif; ?>
+		</div>
+	</div>
+	<button class="overlay__close  js-overlay-trigger"><i class="fa  fa-times-circle-o"></i></button>
+</div><!-- .overlay -->
 
 <?php wp_footer(); ?>
 </body>
