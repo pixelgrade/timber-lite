403b38857cbc690dafe59e3d082aecd23bdfc961
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-07-01 21:57:36 +0300

    functionalized AddThis dummy markup

--- template-parts/addthis-share.php
+++ template-parts/addthis-share.php
@@ -1,11 +1,49 @@
+<?php
+/*
+* Get the comma delimited string from Theme Options and generate the a tags that are needed to make AddThis sharing work
+*
+* @package Timber
+* @since   Timber 1.0
+*/
+
+$share_buttons_types = timber_get_option('share_buttons_settings');
+
+if (!empty($share_buttons_types) || $share_buttons_types !== 'false') :
+//lets go through each button type and create the needed markup
+//but first some cleaning - remove all whitespaces
+$share_buttons_types = preg_replace('/\s+/', '', $share_buttons_types);
+//now take each setting
+$buttons = explode(',',$share_buttons_types);
+//the preferred buttons need to have numbering appended to them
+$preferred_count = 0; ?>
+
 <div class="share-box">
-	<button class="share-button  js-share-button"><i class="fa fa-share-alt"></i></button>
-	<small class="share-text"><?php  echo _('Share:', 'timber'); ?></small>
-	<div class="addthis_toolbox  social-links-list">
-		<a class="addthis_button" href="#"><i class="fa fa-share-alt"></i></a>
-		<a class="addthis_button" href="#"><i class="fa fa-share-alt"></i></a>
-		<a class="addthis_button" href="#"><i class="fa fa-share-alt"></i></a>
-		<a class="addthis_button" href="#"><i class="fa fa-share-alt"></i></a>
-		<a class="addthis_button" href="#"><i class="fa fa-share-alt"></i></a>
-	</div>
-</div>
+    <button class="share-button  js-share-button"><i class="fa fa-share-alt"></i></button>
+    <small class="share-text"><?php  echo _('Share:', 'timber'); ?></small>
+    <div class="addthis_toolbox  social-links-list">
+
+        <?php if ( ! empty( $buttons ) ) {
+            for ( $k = 0; $k < count( $buttons ); $k++ ) {
+                $button_markup = '';
+                switch ($buttons[$k]) {
+                case 'preferred':
+                    $preferred_count++;
+                    $button_markup = '<a class="addthis_button_'.$buttons[$k].'_'.$preferred_count.'"></a>' . PHP_EOL;
+                    break;
+                case 'more':
+                    $button_markup = '<a class="addthis_button_compact"></a>' . PHP_EOL;
+                    break;
+                case 'counter':
+                    $button_markup = '<a class="addthis_counter addthis_bubble_style"></a>' . PHP_EOL;
+                    break;
+                default :
+                    $button_markup = '<a class="addthis_button_'.$buttons[$k].'"></a>' . PHP_EOL;
+                }
+            }
+            echo $button_markup;
+        } ?>
+
+    </div><!-- .addthis_toolbox -->
+</div><!-- .share-box -->
+
+<?php endif; ?>
--- template-parts/content-footer.php
+++ template-parts/content-footer.php
@@ -1,9 +1,11 @@
 <footer id="colophon" class="site-footer" role="contentinfo">
+
     <?php get_template_part('template-parts/addthis-share'); ?>
+
     <div class="site-info">
         <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'timber' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'timber' ), 'WordPress' ); ?></a>
         <span class="sep"> | </span>
         <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'timber' ), 'timber', '<a href="https://pixelgrade.com" rel="designer">Pixelgrade</a>' ); ?>
     </div><!-- .site-info -->
-    <button class="show-button"><span>show thumbnails</span></button>
+    <button class="show-button"><span><?php _e( 'show thumbnails', 'timber' ); ?></span></button>
 </footer><!-- #colophon -->
