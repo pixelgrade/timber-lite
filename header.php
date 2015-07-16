e6f1d46f3c7e2ff2f5a72a8aa89b064e288821ed
Razvan Onofrei <razwan.me@gmail.com>
2015-07-16 11:26:45 +0300

    fixes #97

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 		<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 	</div><!-- .site-branding -->
 
-	<nav id="site-navigation" class="main-navigation" role="navigation">
+	<nav id="site-navigation" class="main-navigation djax-updatable" role="navigation">
 		<?php wp_nav_menu( array(
 			'theme_location' => 'primary',
 			'menu_class' => 'nav nav--main',
