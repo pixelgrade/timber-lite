faf76dd174d4a5a6f7811b2b5984863fbfe1b4b8
Razvan Onofrei <razwan.me@gmail.com>
2015-07-29 12:04:44 +0300

    duplicate

--- functions.php
+++ functions.php
@@ -371,9 +371,6 @@ function timber_last_function(){
 					window.timber_dynamic_loaded_scripts = <?php echo json_encode( $dynamic_scripts ); ?>;
 					window.timber_dynamic_loaded_styles = <?php echo json_encode( $dynamic_styles ); ?>;
 
-					// run this only if we have resources
-					if (!window.hasOwnProperty('timber_static_resources')) return;
-
 					// timber_dynamic_loaded_scripts is generated in footer when all the scripts should be already enqueued
 					$.each( window.timber_dynamic_loaded_scripts, function (key, url) {
 
