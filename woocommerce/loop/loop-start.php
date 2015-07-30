5269570edd320d99ecadf0e2608e77c43013cd45
cristianfrumusanu <cristian.frumusanu@pixelgrade.com>
2015-07-30 13:08:18 +0300

    WooCommerce init — #151

---
+++ woocommerce/loop/loop-start.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Shop/Products Loop Start
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.0.0
+ */
+?>
+<div class="site-header  site-header--placeholder"></div>
+<div class="site-container  site-content  shop">
