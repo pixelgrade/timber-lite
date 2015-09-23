aa5ea047846d0a39c9e3a546cfeeacc52097e118
Razvan Onofrei <razwan.me@gmail.com>
2015-09-23 15:48:03 +0300

    bring da footer back... #204

--- functions.php
+++ functions.php
@@ -198,7 +198,7 @@ function timber_scripts_styles() {
 	global $timber_show_footer;
 
 	$timber_show_footer = false;
-	if ( ! get_post_type() == 'proof_gallery' && ! timber_post_is_project() && ( is_page() || is_single() ) ) {
+	if ( ! (get_post_type() == 'proof_gallery') && ! timber_post_is_project() && ( is_page() || is_single() ) ) {
 		$timber_show_footer = true;
 		$this_template = basename( get_page_template() );
 
@@ -210,7 +210,7 @@ function timber_scripts_styles() {
 			}
 		}
 	}
-	
+
 	// if the woocommerce user wants prettyPhoto, here is the only way it will work.
 
 	if ( ! function_exists( 'is_plugin_active' ) ) {
