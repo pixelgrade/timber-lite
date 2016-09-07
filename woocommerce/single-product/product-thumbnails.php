b576c7515e9fdb27757d0ccb9d21a842e88d9d6c
andreilupu <andrei.lupu@pixelgrade.com>
2016-09-07 10:59:38 +0300

    Update WooCommerce template files
    Fix #338

--- woocommerce/single-product/product-thumbnails.php
+++ woocommerce/single-product/product-thumbnails.php
@@ -2,13 +2,22 @@
 /**
  * Single Product Thumbnails
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/product-thumbnails.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.3.0
+ * @version     2.6.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
 global $post, $product, $woocommerce;
@@ -26,11 +35,13 @@ if ( $attachment_ids ) {
 
 			$classes = array( );
 
-			if ( $loop == 0 || $loop % $columns == 0 )
+			if ( $loop === 0 || $loop % $columns === 0 ) {
 				$classes[] = 'first';
+			}
 
-			if ( ( $loop + 1 ) % $columns == 0 )
+			if ( ( $loop + 1 ) % $columns === 0 ) {
 				$classes[] = 'last';
+			}
 
 			$image_link = wp_get_attachment_url( $attachment_id );
 
