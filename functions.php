5564a2b54bdf45b5072e9bafb63ac2f3ef759e65
andreilupu <andrei.lupu@pixelgrade.com>
2017-04-05 18:40:16 +0300

    fix #355

--- functions.php
+++ functions.php
@@ -553,14 +553,15 @@ add_action( 'admin_enqueue_scripts', 'timber_wp_enqueue_media' );
 
 /* Automagical updates */
 function wupdates_check_JkElr( $transient ) {
-	// Nothing to do here if the checked transient entry is empty
-	if ( empty( $transient->checked ) ) {
+	// First get the theme directory name (the theme slug - unique)
+	$slug = basename( get_template_directory() );
+
+	// Nothing to do here if the checked transient entry is empty or if we have already checked
+	if ( empty( $transient->checked ) || empty( $transient->checked[ $slug ] ) || ! empty( $transient->response[ $slug ] ) ) {
 		return $transient;
 	}
 
 	// Let's start gathering data about the theme
-	// First get the theme directory name (the theme slug - unique)
-	$slug = basename( get_template_directory() );
 	// Then WordPress version
 	include( ABSPATH . WPINC . '/version.php' );
 	$http_args = array (
@@ -628,6 +629,14 @@ function wupdates_check_JkElr( $transient ) {
 	return $transient;
 }
 add_filter( 'pre_set_site_transient_update_themes', 'wupdates_check_JkElr' );
+
+function wupdates_add_id_JkElr( $ids = array() ) {
+	$slug = basename( get_template_directory() );
+	$ids[ $slug ] = array( 'id' => 'JkElr', 'type' => 'theme', );
+
+	return $ids;
+}
+add_filter( 'wupdates_gather_ids', 'wupdates_add_id_JkElr', 10, 1 );
 
 /* Only allow theme updates with a valid Envato purchase code */
 function wupdates_add_purchase_code_field_JkElr( $themes ) {
