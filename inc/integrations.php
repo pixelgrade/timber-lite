29fd7de437f63a67c42c370765a36ceb2e2a940d
George <george.olaru@pixelgrade.com>
2017-04-26 11:52:29 +0300

    Added PixCare integration #365

---
+++ inc/integrations.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Require files that deal with various plugin integrations.
+*
+* @package Timber
+*/
+
+/**
+ * Load PixCare compatibility file.
+ */
+require get_template_directory() . '/inc/integrations/pixcare.php';
