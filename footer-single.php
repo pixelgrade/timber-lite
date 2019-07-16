e04c525c5a04a232d0132fb518dc49c2caa2a8be
madalingorbanescu <madalin@pixelgrade.com>
2019-07-16 16:26:22 +0300

    fixed footer alignment and separator margins

--- footer-single.php
+++ footer-single.php
@@ -6,6 +6,7 @@
  * @since Timber 1.0
  */ ?>
 <footer id="colophon" class="site-footer  site-footer--single">
+    <div class="theme-name"></div>
 	<?php timber_footer_the_copyright() ?>
 	<a href="#top" class="back-to-top"><?php esc_html_e( 'Back to top', 'timber-lite' ); ?></a>
 </footer><!-- #colophon -->
